92b9b2027b0e2299c2751fdc76bbd7f62ee5873c
nutbreaker <10211888+nutbreaker@users.noreply.github.com>
2026-09-10 19:53:03 +0200

    chore: allow next to fetch image from amazon

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3-eu-west-1.amazonaws.com",
+        pathname: "/course.oc-static.com/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
