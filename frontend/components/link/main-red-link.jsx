0b52fae283dc7f6b2f7e37601b8aa9805797b863
nutbreaker <10211888+nutbreaker@users.noreply.github.com>
2026-09-10 20:38:23 +0200

    feat: add main red link

---
+++ frontend/components/link/main-red-link.jsx
@@ -0,0 +1,27 @@
+'use client';
+
+import Link from 'next/link';
+
+import './main-red-link.css';
+
+/**
+ * Custom red link based next/link.
+ *
+ * @param {Object} props all valid next/link props
+ * @param {React.ReactNode} props.children link content
+ * 
+ * @returns {JSX.Element} a Next.js custom red link
+ */
+export default function MainRedLink({
+    children,
+    ...props
+}) {
+    return (
+        <Link
+            {...props}
+            className={`main-red-link ${props.className || ""}`}
+        >
+            {children}
+        </Link>
+    );
+}
