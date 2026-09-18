b391a74f44e93a6a1e5e1b31d90c7f8845554854
nutbreaker <10211888+nutbreaker@users.noreply.github.com>
2026-09-18 16:29:29 +0200

    feat(ajouter-un-logement): restrict client from accessing page

--- frontend/app/(private)/ajouter-un-logement/page.jsx
+++ frontend/app/(private)/ajouter-un-logement/page.jsx
@@ -1,6 +1,27 @@
+import { getSessionToken } from "@/services/session";
+import decodeJWT from "@/utils/jwt-decoder";
+
 import "./page.css";
 
 export default async function AddPropertyPage() {
+  const token = await getSessionToken();
+  const userInfo = await decodeJWT(token);
+
+  if (userInfo?.role === "client") {
+    return (
+      <div
+        className="container add-property-page"
+        style={{ placeContent: "center" }}
+      >
+        <h2>Autorisations insuffisantes</h2>
+        <p>
+          Vous n'avez malheureusement pas lautorisation d'ajouter un nouveau
+          logement.
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div className="container add-property-page">
       <h2>Ajouter une propriété</h2>
